b1830f9f18ec09856a9ddf93c90d9c78e1dbb8a8
Diego-lg <diegoligarrido@gmail.com>
2025-02-26 21:26:27 -0800

    fix postinstall

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
+/* eslint-disable no-var */
 declare global {
-  // Use var here to ensure the prisma instance is truly global
   var prisma: PrismaClient | undefined;
 }
+/* eslint-enable no-var */
 
 const prismadb = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
