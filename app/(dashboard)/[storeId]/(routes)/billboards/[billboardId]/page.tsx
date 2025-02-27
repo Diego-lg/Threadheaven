75ce61419947612e22068d8108f15ac03d41becb
Diego-lg <diegoligarrido@gmail.com>
2025-02-26 19:00:54 -0800

    fix postinstall

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,15 +1,17 @@
 import prismadb from "@/lib/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
-const BillboardPage = async ({
-  params,
-}: {
-  params: { billboardId: string };
-}) => {
+interface BillboardPageProps {
+  params: {
+    billboardId: string;
+  };
+}
+
+const BillboardPage = async ({ params }: BillboardPageProps) => {
   const { billboardId } = params;
   const billboard = await prismadb.billboard.findUnique({
     where: {
-      id: billboardId, // or just `id: billboardId`
+      id: billboardId,
     },
   });
 
